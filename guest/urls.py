da9345a84d051ca0832f3e438aca6c9e121daeb2
shayne-chen <shayne23chen@163.com>
2018-09-05 22:15:00 +0800

    second commit

--- guest/urls.py
+++ guest/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     url(r'^guest_manage/$', views.guest_manage),
     url(r'^search_phone/$', views.search_phone),
     url(r'^sign_index/(?P<event_id>[0-9]+)/$', views.sign_index),
+    url(r'^sign_index_action/(?P<event_id>[0-9]+)/$', views.sign_index_action),
+    url(r'^logout/$', views.logout),
 ]
--- sign/views.py
+++ sign/views.py
@@ -70,3 +70,41 @@ def search_phone(request):
 def sign_index(request, event_id):
 	event = get_object_or_404(Event, id=event_id)
 	return render(request, 'sign_index.html', {"event": event})
+
+@login_required
+def sign_index_action(request, event_id):
+	event = get_object_or_404(Event, id=event_id)
+	phone = request.POST.get('phone', '')
+	print (phone)
+	result = Guest.objects.filter(phone=phone)
+	if not result:
+		return render(request, 'sign_index.html', {'event':event, 'hint':'phone error.'})
+	
+	result = Guest.objects.filter(phone=phone, event_id=event_id)
+	if not result:
+		return render(request, 'sign_index.html', {'event':event, 'hint':'event id or phone error.'})
+
+	result = Guest.objects.get(phone=phone, event_id=event_id)
+	if result.sign:
+		return render(request, 'sign_index.html', {'event':event, 'hint':'user has sign in.'})
+	else:
+		Guest.objects.filter(phone=phone, event_id=event_id).update(sign='1')
+		return render(request, 'sign_index.html', {'event':event, 'hint':'sign in success!', 'guest':result})
+
+
+@login_required
+def logout(request):
+	auth.logout(request)
+	response = HttpResponseRedirect('/index/')
+	return response
+
+
+
+
+
+
+
+
+
+
+
