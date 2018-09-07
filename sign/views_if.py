cd1c483c05631450bab785980a17098736845751
shaw <xiaosadidai@163.com>
2018-09-07 18:03:05 +0800

    interface done

--- sign/views_if.py
+++ sign/views_if.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from sign.models import Event, Guest
-from django.core.exceptions import ValidationError, ObjectsDoesNotExist
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.db.utils import IntegrityError
 import time
 
@@ -49,7 +49,7 @@ def get_event_list(request):
 		event = {}
 		try:
 			result = Event.objects.get(id=event_id)
-		except ObjectsDoesNotExist:
+		except ObjectDoesNotExist:
 			return JsonResponse({"status":10022, "message":"query result is empty"})
 		else:
 			event['name'] = result.name
@@ -117,3 +117,87 @@ def add_guest(request):
 		return JsonResponse({"status":10026, "message":"the event guest phone number repeat"})
 
 	return JsonResponse({"status":200, "message":"add guest success"})
+
+
+
+#嘉宾查询
+def get_guest_list(request):
+	event_id = request.GET.get("event_id", "")
+	phone = request.GET.get("phone", "")
+
+	if event_id == "":
+		return JsonResponse({"status":10021, "message":"event_id can not be empty"})
+
+	if event_id != "" and phone == "":
+		datas = []
+		results = Guest.objects.filter(event_id=event_id)
+		if results:
+			for r in results:
+				guest = {}
+				guest["realname"] = r.realname
+				guest["phone"] = r.phone
+				guest["email"] = r.email
+				guest["sign"] = r.sign
+				datas.append(guest)
+			return JsonResponse({"status":200, "message":"success", "data":datas})
+		else:
+			return JsonResponse({"status":10022, "message":"query result is empty"})
+
+	if event_id != "" and phone != "":
+		guest = []
+		try:
+			result = Guest.objects.get(phone=phone, event_id=event_id)
+		except ObjectDoesNotExist:
+			return JsonResponse({"status":10022, "message":"query result is empty"})
+		else:
+			guest["realname"] = result.realname
+			guest["phone"] = result.phone
+			guest["email"] = result.email
+			guest["sign"] = result.sign
+			return JsonResponse({"status":200, "message":"success", "data":guest})
+
+
+
+#嘉宾签到接口
+def user_sign(request):
+	event_id = request.POST.get("event_id", "")
+	phone = request.POST.get("phone", "")
+
+	if event_id == "" or phone == "":
+		return JsonResponse({"status":10021, "message":"parameter error"})
+
+	result = Event.objects.filter(id=event_id)
+	if not result:
+		return JsonResponse({"status":10022, "message":"event id null"})
+
+	result = Event.objects.get(id=event_id).status
+	if not result:
+		return JsonResponse({"status":10023, "message":"event status is not available"})
+
+	event_time = Event.objects.get(id=event_id).start_time
+	etime = str(event_time).split('.')[0]
+	timeArray = time.strptime(etime, "%Y-%m-%d %H:%M:%S")
+	e_time = int(time.mktime(timeArray))
+
+	now_time = str(time.time())
+	ntime = now_time.split('.')[0]
+	n_time = int(ntime)
+
+	if n_time >= e_time:
+		return JsonResponse({"status":10024, "message":"event has started"})
+
+	result = Guest.objects.filter(phone=phone)
+	if not result:
+		return JsonResponse({"status":10025, "message":"user phone null"})
+
+	result = Guest.objects.filter(event_id=event_id, phone=phone)
+	if not result:
+		return JsonResponse({"status":10026, "message":"user did not participate in the conference"})
+
+	if result:
+		return JsonResponse({"status":10027, "message":"user has sign in"})
+	else:
+		Guest.objects.filter(event_id=event_id, phone=phone).update(sign='1')
+		return JsonResponse({"status":200, "message":"sign success"})
+
+
