3af7ff6448e41837f085099fa7f32338fcaaf5b4
xiao.chen <xiao.chen@mobvoi.com>
2018-12-25 19:28:30 +0800

    pipeline_test_19_28

--- pipeline_test_files/5.sh
+++ pipeline_test_files/5.sh
@@ -3,4 +3,7 @@ cd /var/jenkins_home
 new_filename=`ls -lt core-seg_fault_test*|awk '{print $9}'|head -n 1`
 echo ${new_filename}
 echo "GDB Debug info is below : "
-gdb -c ${new_filename}
+current_time=`date '+%Y-%m-%d %H:%M:%S'`
+gdb -c ${new_filename}>>/var/jenkins_home/core_info_${current_time}.info
+core_file=`ls -lt core_info*|awk '{print $9}'|head -n 1`
+tail -n 20 ${core_file}
