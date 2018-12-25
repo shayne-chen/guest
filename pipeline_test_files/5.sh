a3a91b0f7850354b0c97f97ca96249ebb2514a20
xiao.chen <xiao.chen@mobvoi.com>
2018-12-25 19:18:07 +0800

    pipeline_test_19_18

--- pipeline_test_files/5.sh
+++ pipeline_test_files/5.sh
@@ -2,6 +2,5 @@
 cd /var/jenkins_home
 new_filename=`ls -lt core-seg_fault_test*|awk '{print $9}'|head -n 1`
 echo ${new_filename}
-gdb -c ${new_filename}|tee -a /var/jenkins_home/core_info.log
-
-cat /var/jenkins_home/core_info.log
+echo "GDB Debug info is below : "
+gdb -c ${new_filename}
