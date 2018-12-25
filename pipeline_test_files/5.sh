e5e9992c455d32ae9e56e21dd71f1250d65aa3c9
xiao.chen <xiao.chen@mobvoi.com>
2018-12-25 18:55:55 +0800

    pipeline_test_18_55

--- pipeline_test_files/5.sh
+++ pipeline_test_files/5.sh
@@ -2,4 +2,6 @@
 cd /var/jenkins_home
 new_filename=`ls -lt core-seg_fault_test*|awk '{print $9}'|head -n 1`
 echo ${new_filename}
-gdb -c ${new_filename}
+gdb -c ${new_filename}|tee -a /var/jenkins_home/core_info.log
+
+cat /var/jenkins_home/core_info.log
